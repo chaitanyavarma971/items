a813faa7dc347a240a064199417acd195343c4ab
Chaitanya Varma <chaitanya.varma971@gmail.com>
2021-04-26 20:16:20 -0700

    Add Babel and React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Webpack is working!!!")
+import React, { useState } from "react";
+import { render } from "react-dom";
+
+function App() {
+	const [showMsg, setShowMsg] = useState(false);
+
+	return (
+		<>
+			<button onClick={() => setShowMsg(true)}>Click to see a message!</button>
+			{showMsg && <div>Items will be displayed here...</div>}
+		</>
+	);
+}
+
+render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+		],
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, "src", "index.html"),
